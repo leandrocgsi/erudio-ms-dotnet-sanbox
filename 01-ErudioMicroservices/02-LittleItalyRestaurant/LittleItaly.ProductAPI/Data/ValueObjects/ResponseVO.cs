b01014ff8d8bb607c0820ca05435f235d19601d0
leandrocgsi <leandrocgsi@gmail.com>
2021-08-04 19:30:22 -0300

    Creating our First Microservice

---
+++ 01-ErudioMicroservices/02-LittleItalyRestaurant/LittleItaly.ProductAPI/Data/ValueObjects/ResponseVO.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace LittleItaly.ProductAPI.Data.ValueObjects
+{
+    public class ResponseVO
+    {
+        public bool IsSuccess {  get; set; } =  true;
+        public object Result { get; set; }
+        public string DisplayMessage { get; set; } = "";
+        public List<string> ErrorMessages { get; set; }
+    }
+}
---
+++ 01-ErudioMicroservices/02-LittleItalyRestaurant/LittleItaly.ProductAPI/Repository/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using LittleItaly.ProductAPI.Data.ValueObjects;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LittleItaly.ProductAPI.Repository
+{
+    public  interface IProductRepository
+    {
+        Task<IEnumerable<ProductVO>> GetProducts();
+        Task<ProductVO> GetProductById(long productId);
+        Task<ProductVO> CreateOrUpdateProduct(ProductVO productVO);
+        Task<bool> DeleteProduct(long productId);
+    }
+}
