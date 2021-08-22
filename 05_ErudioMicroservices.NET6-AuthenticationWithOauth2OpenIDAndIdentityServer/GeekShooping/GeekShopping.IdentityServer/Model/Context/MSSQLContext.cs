f4f898b391b0371bb58421f501c45d47cf29f1b2
leandrocgsi <leandrocgsi@gmail.com>
2021-08-22 15:27:34 -0300

    Authentication with Oauth2, OpenID, Duende and Identity Server - SQL Server

---
+++ 05_ErudioMicroservices.NET6-AuthenticationWithOauth2OpenIDAndIdentityServer/GeekShooping/GeekShopping.IdentityServer/Model/Context/MSSQLContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.IdentityServer.Model.Context
+{
+    public class MSSQLContext : IdentityDbContext<ApplicationUser>
+    {
+        public MSSQLContext()
+        {
+
+        }
+        public MSSQLContext(DbContextOptions<MSSQLContext> options)
+            : base(options) { }
+    }
+}
