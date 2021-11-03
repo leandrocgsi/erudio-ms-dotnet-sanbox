3405df31a8b9fc595aefb46fdd37f19153dcb4d8
leandrocgsi <leandrocgsi@gmail.com>
2021-11-03 17:37:25 -0300

    Consuming Shopping Cart Microsservice

---
+++ 12_ErudioMicroservices.NET6-ConsumingShoppingCartMicrosservice/GeekShooping/GeekShopping.CartAPI/Controllers/CartController.cs
@@ -0,0 +1,51 @@
+﻿using GeekShopping.CartAPI.Data.ValueObjects;
+using GeekShopping.CartAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace GeekShopping.CartAPI.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class CartController : Controller
+    {
+        private readonly ICartRepository _repository;
+
+        public CartController(ICartRepository cartRepository)
+        {
+            _repository = cartRepository;
+        }
+
+        [HttpGet("find-cart/{userId}")]
+        public async Task<ActionResult<CartVO>> FindCartByUserId(string userId)
+        {
+            var cart = await _repository.FindCartByUserId(userId);
+            if (cart == null) return NotFound();
+            return Ok(cart);
+        }
+
+        [HttpPost("add-cart")]
+        public async Task<ActionResult<CartVO>> AddCart(CartVO vo)
+        {
+            var cart = await _repository.SaveOrUpdateCart(vo);
+            if (cart == null) return NotFound();
+            return Ok(cart);
+        }
+
+        [HttpPut("update-cart")]
+        public async Task<ActionResult<CartVO>> UpdateCart(CartVO vo)
+        {
+            var cart = await _repository.SaveOrUpdateCart(vo);
+            if (cart == null) return NotFound();
+            return Ok(cart);
+        }
+
+        [HttpDelete("remove-cart/{id}")]
+        public async Task<ActionResult<CartVO>> RemoveCart(int id)
+        {
+            var status = await _repository.RemoveFromCart(id);
+            if (!status) return BadRequest();
+            return Ok(status);
+        }
+    }
+}
