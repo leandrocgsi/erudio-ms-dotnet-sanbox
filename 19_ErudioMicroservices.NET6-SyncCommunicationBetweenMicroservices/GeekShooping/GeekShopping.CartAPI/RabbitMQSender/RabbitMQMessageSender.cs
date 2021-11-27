8c908e00584646d96885261fb84792cf386123c0
leandrocgsi <leandrocgsi@gmail.com>
2021-11-27 15:20:48 -0300

    Sync Communication Between Microservices with RabbitMQ

---
+++ 19_ErudioMicroservices.NET6-SyncCommunicationBetweenMicroservices/GeekShooping/GeekShopping.CartAPI/RabbitMQSender/RabbitMQMessageSender.cs
@@ -0,0 +1,74 @@
+﻿using GeekShopping.CartAPI.Messages;
+using GeekShopping.MessageBus;
+using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Text.Json;
+
+namespace GeekShopping.CartAPI.RabbitMQSender
+{
+    public class RabbitMQMessageSender : IRabbitMQMessageSender
+    {
+        private readonly string _hostname;
+        private readonly string _password;
+        private readonly string _username;
+        private IConnection _connection;
+
+        public RabbitMQMessageSender()
+        {
+            _hostname = "localhost";
+            _password = "guest";
+            _username = "guest";
+        }
+
+        public void SendMessage(BaseMessage message, string queueName)
+        {
+            if (ConnectionExists())
+            {
+                using var channel = _connection.CreateModel();
+                channel.QueueDeclare(queue: queueName, false, false, false, arguments: null);
+
+                byte[] body = GetMessageAsByteArray(message);
+                channel.BasicPublish(exchange: "", routingKey: queueName, basicProperties: null, body: body);
+            }
+        }
+
+        private static byte[] GetMessageAsByteArray(BaseMessage message)
+        {
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true
+            };
+
+            var json = JsonSerializer.Serialize<CheckoutHeaderVO>((CheckoutHeaderVO)message, options);
+            var body = Encoding.UTF8.GetBytes(json);
+            return body;
+        }
+
+        private void CreateConnection()
+        {
+            try
+            {
+                var factory = new ConnectionFactory
+                {
+                    HostName = _hostname,
+                    UserName = _username,
+                    Password = _password
+                };
+                _connection = factory.CreateConnection();
+            }
+            catch (Exception)
+            {
+                //log exception
+            }
+        }
+
+        private bool ConnectionExists()
+        {
+            if (_connection != null) return true;
+
+            CreateConnection();
+            return _connection != null;
+        }
+    }
+}
