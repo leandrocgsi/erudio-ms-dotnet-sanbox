ea26c362ca9ebc1cf731929b5c4cd00f21ad71d5
leandrocgsi <leandrocgsi@gmail.com>
2021-11-29 14:45:25 -0300

    Processing Queues on RabbitMQ

---
+++ 22_ErudioMicroservices.NET6-ProcessingQueuesOnRabbitMQ/GeekShooping/GeekShopping.PaymentAPI/Program.cs
@@ -0,0 +1,21 @@
+using GeekShopping.PaymentAPI.Messages;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace GeekShopping.PaymentAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
