16ed232d790ce2deb4aa562bfcc3d7e4230ec3df
leandrocgsi <leandrocgsi@gmail.com>
2021-11-30 17:46:41 -0300

    Working with Exchanges in RabbitMQ - Fanout

---
+++ 24A_ErudioMicroservices.NET6-WorkingWithExchangesInRabbitMQ/GeekShooping/GeekShopping.Email/Model/EmailLog.cs
@@ -0,0 +1,22 @@
+﻿using GeekShopping.Email.Model.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeekShopping.Email.Model
+{
+    [Table("email_logs")]
+    public class EmailLog : BaseEntity
+    {
+        [Column("email")]
+        public string Email { get; set; }
+
+        [Column("log")]
+        public string Log { get; set; }
+
+        [Column("sent_date")]
+        public DateTime SentDate { get; set; }
+    }
+}
