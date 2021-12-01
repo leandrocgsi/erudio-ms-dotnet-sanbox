8516adfc98af21ff17201071ae0a0b985d4bdd91
leandrocgsi <leandrocgsi@gmail.com>
2021-12-01 18:31:46 -0300

    API Gateway with Ocelot

--- 25_ErudioMicroservices.NET6-WorkingWithOcelot/GeekShooping/GeekShopping.APIGateway/Startup.cs
+++ 25_ErudioMicroservices.NET6-WorkingWithOcelot/GeekShooping/GeekShopping.APIGateway/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -41,23 +42,14 @@ namespace GeekShopping.APIGateway
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            await app.UseOcelot();
         }
     }
 }
