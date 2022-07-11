27ae47904c63a6332e43d40054fd41cf3526f6a1
leandrocgsi <leandrocgsi@gmail.com>
2022-07-11 11:16:30 -0300

    Changing to Final Release of .NET 6

---
+++ FinalRelease/GeekShopping.APIGateway/Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = "https://localhost:4435/";
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false
+        };
+    });
+
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+app.UseOcelot();
+
+app.Run();
