d5cb3f22b335a45f011fd22f2ba83e459dcc9eae
leandrocgsi <leandrocgsi@gmail.com>
2022-07-17 11:18:15 -0300

    Changing to Final Release of .NET 6

---
+++ FinalRelease/GeekShopping.Integration/Enuns/RabbitMQIntegration.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+
+namespace GeekShopping.Integration.Enuns
+{
+    public enum QueueName
+    {
+        [Description("checkout")]
+        Checkout,
+
+        [Description("order_payment_process")]
+        OrderPaymentProcess,
+
+        [Description("order_payment_result")]
+        OrderPaymentResult,
+
+        [Description("payment_email_update")]
+        PaymentEmailUpdate,
+
+        [Description("payment_order_update")]
+        PaymentOrderUpdate
+    }
+
+    public enum ExchangeName
+    {
+        [Description("direct_payment_update")]
+        DirectPaymentUpdate
+    }
+
+    public enum RoutingKey
+    {
+        [Description("PaymentEmail")]
+        PaymentEmail,
+
+        [Description("PaymentOrder")]
+        PaymentOrder
+    }
+}
