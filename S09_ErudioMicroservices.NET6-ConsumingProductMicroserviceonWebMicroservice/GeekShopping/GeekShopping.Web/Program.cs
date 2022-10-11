15e0d0367f094b32f83e5eaeb9ac9d9a4e00030d
leandrocgsi <leandrocgsi@gmail.com>
2022-10-11 15:22:39 -0300

    Consuming Product Microservice on Web Microservice

--- S09_ErudioMicroservices.NET6-ConsumingProductMicroserviceonWebMicroservice/GeekShopping/GeekShopping.Web/Program.cs
+++ S09_ErudioMicroservices.NET6-ConsumingProductMicroserviceonWebMicroservice/GeekShopping/GeekShopping.Web/Program.cs
@@ -1,9 +1,16 @@
+using GeekShopping.Web.Services.IServices;
+using GeekShopping.Web.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
+
+builder.Services.AddHttpClient<IProductService, ProductService>(
+    c => c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:ProductAPI"])
+);
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
