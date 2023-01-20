d860ca110a7b8fae8126d5675e19009af9d98bfb
leandrocgsi <leandrocgsi@gmail.com>
2023-01-20 11:13:08 -0300

    Authentication with Oauth2, OpenID, Duende and Identity Server - Adding Duende Scaffold

--- S11_ErudioMicroservices.NET6-AuthenticationWithOauth2OpenIDAndIdentityServer/GeekShopping/GeekShopping.IdentityServer/Program.cs
+++ S11_ErudioMicroservices.NET6-AuthenticationWithOauth2OpenIDAndIdentityServer/GeekShopping/GeekShopping.IdentityServer/Program.cs
@@ -1,3 +1,12 @@
+using Duende.IdentityServer.Services;
+using GeekShopping.IdentityServer.Configuration;
+using GeekShopping.IdentityServer.Initializer;
+using GeekShopping.IdentityServer.Model;
+using GeekShopping.IdentityServer.Model.Context;
+using GeekShopping.IdentityServer.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 namespace GeekShopping.IdentityServer
 {
     public class Program
@@ -6,10 +15,41 @@ namespace GeekShopping.IdentityServer
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            var connection = builder.Configuration["MySqlConnection:MySqlConnectionString"];
+
+            builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(
+                connection,
+                new MySqlServerVersion(new Version(8, 0, 29)))
+            );
+
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<MySQLContext>()
+                .AddDefaultTokenProviders();
+
+            var builderServices = builder.Services.AddIdentityServer(options =>
+            {
+                options.Events.RaiseErrorEvents = true;
+                options.Events.RaiseInformationEvents = true;
+                options.Events.RaiseFailureEvents = true;
+                options.Events.RaiseSuccessEvents = true;
+                options.EmitStaticAudienceClaim = true;
+            })
+                .AddInMemoryIdentityResources(IdentityConfiguration.IdentityResources)
+                .AddInMemoryApiScopes(IdentityConfiguration.ApiScopes)
+                .AddInMemoryClients(IdentityConfiguration.Clients)
+                .AddAspNetIdentity<ApplicationUser>();
+
+            builder.Services.AddScoped<IDbInitializer, DbInitializer>();
+            builder.Services.AddScoped<IProfileService, ProfileService>();
+
+            builderServices.AddDeveloperSigningCredential();
+
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
+
+            var dbInitializerService = app.Services.CreateScope().ServiceProvider.GetService<IDbInitializer>();
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
@@ -24,7 +64,11 @@ namespace GeekShopping.IdentityServer
 
             app.UseRouting();
 
+            app.UseIdentityServer();
+
             app.UseAuthorization();
+
+            dbInitializerService.Initialize();
 
             app.MapControllerRoute(
                 name: "default",
