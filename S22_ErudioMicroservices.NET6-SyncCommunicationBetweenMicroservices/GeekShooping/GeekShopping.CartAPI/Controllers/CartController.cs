654984af300758b77ebf07b79ddae602cde2a8be
leandrocgsi <leandrocgsi@gmail.com>
2023-01-24 18:58:37 -0300

    Sync Communication Between Microservices

--- S22_ErudioMicroservices.NET6-SyncCommunicationBetweenMicroservices/GeekShooping/GeekShopping.CartAPI/Controllers/CartController.cs
+++ S22_ErudioMicroservices.NET6-SyncCommunicationBetweenMicroservices/GeekShooping/GeekShopping.CartAPI/Controllers/CartController.cs
@@ -2,6 +2,7 @@
 using GeekShopping.CartAPI.Messages;
 using GeekShopping.CartAPI.RabbitMQSender;
 using GeekShopping.CartAPI.Repository;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -80,7 +81,8 @@ namespace GeekShopping.CartAPI.Controllers
         [HttpPost("checkout")]
         public async Task<ActionResult<CheckoutHeaderVO>> Checkout(CheckoutHeaderVO vo)
         {
-            string token = Request.Headers["Authorization"];
+            // string token = Request.Headers["Authorization"];
+            var token = await HttpContext.GetTokenAsync("access_token");
 
             if (vo?.UserId == null) return BadRequest();
             var cart = await _cartRepository.FindCartByUserId(vo.UserId);
