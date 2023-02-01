13860405535d0fa4a438e9ef62330e5564c371b3
leandrocgsi <leandrocgsi@gmail.com>
2023-02-01 18:59:13 -0300

    Creating Microservice GeekShopping.PaymentAPI

--- S23_ErudioMicroservices.NET6-CreatingPaymentAPIMicroservice/GeekShooping/GeekShopping.CouponAPI/Controllers/CouponController.cs
+++ S23_ErudioMicroservices.NET6-CreatingPaymentAPIMicroservice/GeekShooping/GeekShopping.CouponAPI/Controllers/CouponController.cs
@@ -1,11 +1,9 @@
 ﻿using GeekShopping.CouponAPI.Data.ValueObjects;
 using GeekShopping.CouponAPI.Repository;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace GeekShopping.CouponAPI.Controllers
@@ -24,8 +22,10 @@ namespace GeekShopping.CouponAPI.Controllers
         }
 
         [HttpGet("{couponCode}")]
+        [Authorize]
         public async Task<ActionResult<CouponVO>> GetCouponByCouponCode(string couponCode)
         {
+            // var token = await HttpContext.GetTokenAsync("access_token");
             var coupon = await _repository.GetCouponByCouponCode(couponCode);
             if (coupon == null) return NotFound();
             return Ok(coupon);
